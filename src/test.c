b27c43f51eeac46ad3459252c16341d9c40230e0
Johan Stokking <johan@stokking.nl>
2016-11-02 17:51:06 +0100

    Update test

--- src/test.c
+++ src/test.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <signal.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 #include "connector.h"
 
 int running = 1;
@@ -11,13 +14,19 @@ void stop(int sig)
 	running = 0;
 }
 
+void downlink(Router__DownlinkMessage *msg, void *arg)
+{
+	printf("Got downlink\n");
+}
+
 int main(int argc, char **argv) {
+	srand(time(NULL));
 	signal(SIGINT, stop);
 	signal(SIGTERM, stop);
 
 	// Initialize the TTN gateway with ID office
 	TTN *ttn;
-	ttngwc_init(&ttn, "office");
+	ttngwc_init(&ttn, "office", &downlink, NULL);
 	if (!ttn) {
 		printf("Failed to initialize TTN gateway\n");
 		return -1;
@@ -36,20 +45,19 @@ int main(int argc, char **argv) {
 	// Start the loop
 	int i = 0;
 	while (running) {
-		// Handle data. You want to do this on a thread
-		err = ttngwc_yield(ttn);
-		if (err != 0) {
-			break;
-		}
-
 		// Send gateway status. The only parameter we're sending is the time
-		err = ttngwc_send_status(ttn, ++i);
+		Gateway__Status status = GATEWAY__STATUS__INIT;
+		status.has_time = 1;
+		status.time = ++i;
+
+		err = ttngwc_send_status(ttn, &status);
 		if (err != 0) {
 			printf("Send status failed: %d\n", err);
 			break;
 		}
 
 		printf("Published time %d\n", i);
+		sleep(rand() % 20);
 	}
 
 	printf("Disconnecting\n");
