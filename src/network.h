c92b63c9d5b785cfde9731099e979d0494ca37a0
Johan Stokking <johan@stokking.nl>
2016-11-30 22:22:15 +0800

    Include clang format for LLVM and formatted source code, closes #7

--- src/network.h
+++ src/network.h
@@ -9,14 +9,14 @@
 #include "github.com/TheThingsNetwork/ttn/api/router/router.pb-c.h"
 
 #define KEEP_ALIVE_INTERVAL 20
-#define COMMAND_TIMEOUT     1000
-#define READ_BUFFER_SIZE    100
-#define SEND_BUFFER_SIZE    100
+#define COMMAND_TIMEOUT 1000
+#define READ_BUFFER_SIZE 100
+#define SEND_BUFFER_SIZE 100
 
-#define QOS_STATUS   QOS1
-#define QOS_DOWN     QOS1
-#define QOS_UP       QOS1
-#define QOS_CONNECT  QOS1
-#define QOS_WILL     QOS1
+#define QOS_STATUS QOS1
+#define QOS_DOWN QOS1
+#define QOS_UP QOS1
+#define QOS_CONNECT QOS1
+#define QOS_WILL QOS1
 
 #endif
--- src/session.h
+++ src/session.h
@@ -4,13 +4,13 @@
 #include <MQTTClient.h>
 
 struct Session {
-	Network network;
-	MQTTClient client;
-	TTNDownlinkHandler downlink_handler;
-	void *cb_arg;
-	unsigned char *read_buffer;
-	unsigned char *send_buffer;
-	char *id;
+  Network network;
+  MQTTClient client;
+  TTNDownlinkHandler downlink_handler;
+  void *cb_arg;
+  unsigned char *read_buffer;
+  unsigned char *send_buffer;
+  char *id;
 };
 
 #endif
